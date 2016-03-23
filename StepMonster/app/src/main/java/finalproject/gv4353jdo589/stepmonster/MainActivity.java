860cf38a18d1cf5ec215630154fca35cb5729ef7
Garner Vincent <garnervincent@gmail.om>
2016-03-22 19:27:28 -0500

    Initial Commit

---
+++ StepMonster/app/src/main/java/finalproject/gv4353jdo589/stepmonster/MainActivity.java
@@ -0,0 +1,13 @@
+package finalproject.gv4353jdo589.stepmonster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
