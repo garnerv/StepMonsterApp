0529b8b3edec47d6350e6e1d55ba2baaf89c6ce8
savethequinn <justinowens@utexas.edu>
2016-03-25 01:08:42 -0500

    Push 3.25.1AM
    Added a bunch of shit

---
+++ StepMonster/app/src/main/java/finalproject/gv4353jdo589/stepmonster/ProfileView.java
@@ -0,0 +1,13 @@
+package finalproject.gv4353jdo589.stepmonster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfileView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_view);
+    }
+}
